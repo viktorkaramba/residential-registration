685b52d5575198793c970684a266e6bdc3bd2644
Viktor Svynar <svynarvikror@gmail.com>
2024-03-01 14:09:40 +0200

    fix

--- backend/internal/handlers/OSBB.go
+++ backend/internal/handlers/OSBB.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -416,7 +415,7 @@ func (h *Handler) getAllPollsAnswers(c *gin.Context) {
 	if err != nil {
 		logger.Error("failed to parse osbb id", "error", err)
 		h.sendErrResponse(c, h.Logger,
-			errs.Err(fmt.Errorf("failed to parse osbb id: %w", err)).Code("Failed parse param").Kind(errs.Validation))n
+			errs.Err(fmt.Errorf("failed to parse osbb id: %w", err)).Code("Failed parse param").Kind(errs.Validation))
 		return
 	}
 
