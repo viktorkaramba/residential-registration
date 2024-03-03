f5cf934f82e896e433971d5fe1e0717719a05779
Viktor Svynar <svynarvikror@gmail.com>
2024-03-03 13:21:31 +0200

    added token header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -7,7 +7,10 @@ const Header = () =>{
     const searchTerm = "osbb/1/inhabitants";
     const getData = useCallback(async() =>{
         try {
-            await fetch("http://20.52.189.179:80/osbb/1/inhabitants")
+            const request = {
+                headers: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDk1MDc1NTYsImlhdCI6MTcwOTQ2NDM1NiwiVXNlcklEIjo0fQ.QTNBBnxyjMWPuWO8NVSb8KQhgjMLGou4D--uAnEPE-0' }
+            };
+            await fetch("http://20.52.189.179:80/osbb/1/announcements", request)
                 .then(response => response.json())
                 .then(data => console.log(data));
         }catch (error){
