804a1e8a38d968fda8fc4618fdbba9a00367d12d
Viktor Svynar <61514899+viktorkaramba@users.noreply.github.com>
2024-03-05 15:55:56 +0200

    Feature/backend (#10)
    * added get all osbbs endpoint
    
    * added get osbb profile

--- backend/internal/services/storages.go
+++ backend/internal/services/storages.go
@@ -25,7 +25,7 @@ type BuildingStorage interface {
 type OSBBStorage interface {
 	CreateOSBB(OSBB *entity.OSBB) error
 	ListOSBBS(filter OSBBFilter) ([]entity.OSBB, error)
-	GetOSBB(OSBBID uint64) (*entity.OSBB, error)
+	GetOSBB(filter OSBBFilter) (*entity.OSBB, error)
 	CreateAnnouncement(announcement *entity.Announcement) error
 	ListAnnouncements(filter AnnouncementFilter) ([]entity.Announcement, error)
 	CreatePoll(poll *entity.Poll) error
@@ -60,6 +60,7 @@ type PollFilter struct {
 }
 
 type OSBBFilter struct {
+	OSBBID            *uint64
 	WithBuilding      bool
 	WithAnnouncements bool
 	WithOSBBHead      bool
