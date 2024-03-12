d29dbdcbdf1e2334a673c9d1fb708ddec3ba6735
Viktor Svynar <61514899+viktorkaramba@users.noreply.github.com>
2024-03-12 12:59:23 +0200

    Feature/backend (#16)
    * added get all osbbs endpoint
    
    * added get osbb profile
    
    * added get osbb profile
    
    * fix
    
    * added update and delete methods for poll and test answer
    
    * decompose generate hash password in single function
    
    * added handling unique values
    
    * added updating user answer
    
    * added get list user answers
    
    * fix bug, added err

--- backend/internal/handlers/auth.go
+++ backend/internal/handlers/auth.go
@@ -114,7 +114,8 @@ func (h *Handler) refreshToken(c *gin.Context) {
 	}
 	if oldToken.Revoked {
 		logger.Error("failed to refresh token", "error", errors.New("token is revoked"))
-		h.sendErrResponse(c, h.Logger, fmt.Errorf("failed to refresh token: %w", errors.New("token is revoked")))
+		h.sendErrResponse(c, h.Logger, fmt.Errorf("failed to refresh token: %w",
+			errs.Err(errors.New("token is revoked")).Code("Token is revoked").Kind(errs.Private)))
 		return
 	}
 	newToken, err := h.Services.Token.RefreshToken(oldToken.UserID)
