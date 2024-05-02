b7348d4ac3c7263e63ced1d6f9e88c2aebc1e86f
Viktor Svynar <61514899+viktorkaramba@users.noreply.github.com>
2024-05-02 18:20:38 +0300

    Feature/frontend (#41)
    * added test api request
    
    * added token header
    
    * added osbb components
    
    * added form for adding osbbs and inhabitants
    
    * added form for adding osbbs and inhabitants
    
    * added test poll forms
    
    * added poll delete and update function
    
    * added id for poll
    
    * added poll interface for user
    
    * added refresh token method
    
    * added error handling
    
    * added user and osbb profile
    
    * added delete and update user answer methods
    
    * added delete and update announcements
    
    * refactor update items and error handling
    
    * added user wait approve
    
    * decorate interface
    
    * decorate interface
    
    * decorate interface
    
    * fix error handling, add photo, fix some issues
    
    * add poll results
    
    * add apartment form
    
    * implemented osbb head only update and not approved check
    
    * add check date
    
    * add default photo and correct redirect
    
    * fix main.yml
    
    * added focus on div and user already exist error
    
    * fixed redirect to profile
    
    * added payment
    
    * added payment filter by payment status and user
    
    * added filter and sort functionality for payment

--- frontend/src/components/Payment/PaymentUser/PaymentUserItem.tsx
+++ frontend/src/components/Payment/PaymentUser/PaymentUserItem.tsx
@@ -19,9 +19,10 @@ const PaymentUserItem = ({payment, updatePurchase}:any) => {
         if(user.role === 'osbb_head'){
             isOsbbHead="-osbb-head";
         }
-        fetch(config.apiUrl+'osbb/'+osbbID+'/payments/'+payment.payment_id+'/purchase'+isOsbbHead, requestOptions)
+        fetch(config.apiUrl+'osbb/'+osbbID+'/payments/'+payment.payment_id+'/purchase'  , requestOptions)
             .then(response => response.json())
             .then(data => {
+                console.log(data)
                 if(data){
                     const {error}:any = data;
                     if(error){
@@ -51,12 +52,12 @@ const PaymentUserItem = ({payment, updatePurchase}:any) => {
                     </div>
                 </div>
                 </div>
-            {payment.payment_status==="not_paid" && user.id === payment.user_id && <div>
+            {payment.payment_status==="not_paid" && user?.id === payment.user_id && <div>
                 <button className='button announcement_button' style={{marginRight:'5px'}} type="submit" onClick={()=>updatePurchaseRequest(payment.purchase_id, "paid")} name="to_result">
                     <span className="button_content poll_button_content_form">Оплатити</span>
                 </button>
             </div>}
-            {payment.payment_status==="not_paid" && user.id !== payment.user_id && <div>
+            {payment.payment_status==="not_paid" && user?.id !== payment.user_id && <div>
                 <div className='announcements-item-info-item fw-7 fs-26'>
                     <span>Не сплачено</span>
                 </div>
